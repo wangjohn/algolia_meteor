5d07fb1cde139328d4baa9567148a71f580f74a1
wangjohn <wangjohn@mit.edu>
2014-04-14 11:47:46 -0400

    Changing the export.

--- package.js
+++ package.js
@@ -3,9 +3,6 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-
-  // ensure backwards compatibility with Meteor pre-0.6.5
-  if (api.export) api.export('ALGOLIA_METEOR');
-
   api.add_files('algoliasearch.js', 'client');
+  api.export('AlgoliaSearch', 'client');
 });
